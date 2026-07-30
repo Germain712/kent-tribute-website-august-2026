0cf412990799fa0a13df26492087c00ee2664aec
Reuben <Germain712@outlook.com>
2026-07-30 18:56:37 +0100

    intial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Mobile nav toggle
+const navToggle = document.querySelector('.nav-toggle');
+const mainNav = document.getElementById('main-nav');
+
+if (navToggle && mainNav) {
+  navToggle.addEventListener('click', () => {
+    const isOpen = mainNav.classList.toggle('open');
+    navToggle.setAttribute('aria-expanded', String(isOpen));
+  });
+}
