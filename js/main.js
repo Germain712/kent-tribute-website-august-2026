2c38de4438f74b81499cd9527bc4f9cf733a600f
Reuben <Germain712@outlook.com>
2026-08-12 03:05:42 +0100

    made some improvements

--- js/main.js
+++ js/main.js
@@ -3,8 +3,17 @@ const navToggle = document.querySelector('.nav-toggle');
 const mainNav = document.getElementById('main-nav');
 
 if (navToggle && mainNav) {
+  const closeMenu = () => {
+    mainNav.classList.remove('open');
+    navToggle.setAttribute('aria-expanded', 'false');
+  };
+
   navToggle.addEventListener('click', () => {
     const isOpen = mainNav.classList.toggle('open');
     navToggle.setAttribute('aria-expanded', String(isOpen));
+  });
+
+  mainNav.querySelectorAll('a').forEach((link) => {
+    link.addEventListener('click', closeMenu);
   });
 }
